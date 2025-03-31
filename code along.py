1b97a6a7250ac28b89073e5df6c8ac2bb3f4503a
oladayotimileyin <olatimi4real@gmail.com>
2025-03-31 23:28:20 +0100

    added new lines

--- code along.py
+++ code along.py
@@ -12,3 +12,26 @@ print(weather)
 weather= "It\'s \"kind of\" sunny \n hope you have a good day!"
 
 print(weather)
+
+##formated string - add f in the beginning
+
+name = 'Timileyin'
+Age = 32
+
+print(f'Hi! {name}, you are {Age} year old today')
+
+## string indexing
+
+## variable[start:stop:stepover]
+selfish = 'me me me'
+
+print(selfish[:4:2])
+
+#reverse order
+print(selfish[::-1])
+
+##immutability
+
+#strings are immutable, you can reasign a new value to an index of a string
+
+
