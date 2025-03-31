3a2012c35ba5aae6638c2dfa599263cecdcf349f
oladayotimileyin <olatimi4real@gmail.com>
2025-03-31 23:03:42 +0100

    added new Lines

---
+++ code along.py
@@ -0,0 +1,14 @@
+##type conversion
+print(type(str(100)))
+
+##escape sequence
+## add \
+
+weather= "It\'s \"kind of\" sunny"
+
+print(weather)
+
+# you can add a new line
+weather= "It\'s \"kind of\" sunny \n hope you have a good day!"
+
+print(weather)
