2e987a2a7efa2577fafdd318ada34e9a97c10af5
oladayotimileyin <olatimi4real@gmail.com>
2025-06-04 22:29:27 +0100

    added new file

---
+++ Functions parameters.py
@@ -0,0 +1,8 @@
+
+## parameters
+def sayhello(name, emoji):
+    print(f"Hello {name} {emoji}")
+
+#3 arguments are the actual values we provide to the function when we call it
+sayhello("John", "😊")
+sayhello("Juliet", "😊")
---
+++ findduplicate exercise.py
@@ -0,0 +1,11 @@
+list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
+
+duplicates = []
+
+for item in list:
+    if list.count(item) > 1:
+        if item not in duplicates:
+            duplicates.append(item)
+
+
+print(duplicates)
