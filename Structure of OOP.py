21aaac1dc0408c733b2c4560a08cd8cce89fc761
oladayotimileyin <olatimi4real@gmail.com>
2025-06-10 21:56:16 +0100

    new file

---
+++ Structure of OOP.py
@@ -0,0 +1,18 @@
+Class NameOfClass:
+    ClassAttrribute = 'Value'
+    def __init__(self, param1, param2):
+        self.param1 = param1
+        self.param2 = param2
+
+    def method1(self):
+        #CODE
+    
+    @classmethod
+    def class_method(cls):
+        #CODE
+        pass
+    
+    @staticmethod
+    def static_method():
+        #CODE
+        pass
